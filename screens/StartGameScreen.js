4d5f9f4e80a76ddc715f8fffe3bb1a1db887c55b
Kajal Chakole <kajal.chakole@getvymo.com>
2024-03-07 01:11:59 +0530

    improved button styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,28 +4,36 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCorrect={false}/>
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCorrect={false} />
+
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
-        padding:16,
+        padding: 16,
         marginHorizontal: 24,
         marginTop: 100,
-        borderRadius:8,
+        borderRadius: 8,
         backgroundColor: '#4e0329',
-        elevation:4,
+        elevation: 4,
         shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2},
-        shadowRadius:6,
-        shadowOpacity:0.25,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+        alignItems: 'center',
     },
     numberInput: {
-        height:50,
+        height: 50,
         width: 50,
         fontSize: 32,
         borderBottomColor: '#ddb52f',
@@ -34,6 +42,12 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center',
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer: {
+        flex: 1,
     }
 });
 
